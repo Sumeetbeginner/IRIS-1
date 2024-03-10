ca2dfd8a000cc45b536a9d336797866afb8f71c9
Sumeet Gupta <sumeetgupta3690@gmail.com>
2024-03-10 21:15:58 +0530

    Update Frontend Register and Login

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -1,10 +1,40 @@
 
 import React from 'react'
+import './authentication.css';
+import {useNavigate} from 'react-router-dom'
+import appLogo from '../../assets/images/irislight.png'
 
 const Login = () => {
+
+  const location = useNavigate()
+
+  const changePath = (path) =>{
+    location('/' + path)
+}
+
+
   return (
-    <div>
-      
+    <div className='regBg'>
+      <div className="regBox">
+        <h2 className='loginH'>Login</h2>
+
+        <img className='appL' src={appLogo} alt="" />
+
+        <select className='slct' name="userType" id="">
+          <option value="" disabled selected>Select User Type</option>
+          <option value="student">Student</option>
+          <option value="institution">Institution</option>
+        </select>
+
+        <input type="text" placeholder='Enter Registered Email ID' className='inp'/>
+        <input type="password" placeholder='Enter Password' className='inp'/>
+
+        <button className='btn'>Login</button>
+
+        
+        <p className='alreadyR'>Not a User?<span onClick={() => changePath('registration')}> Register</span> </p>
+      </div>
+
     </div>
   )
 }
--- client/src/components/Authentication/Registration.jsx
+++ client/src/components/Authentication/Registration.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './authentication.css';
 import userPic from '../../assets/images/student.png';
 import universityPic from '../../assets/images/university.png';
+import {useNavigate} from 'react-router-dom'
 
 const Registration = () => {
-    const [userType, setUserType] = useState('______________');
+    const [userType, setUserType] = useState('');
     const [noUserType, setNoUserType] = useState('');
     const [stepNo, setStepNo] = useState(1);
 
@@ -12,6 +13,12 @@ const Registration = () => {
         setUserType(input);
         setNoUserType(input2)
     };
+
+    const location = useNavigate()
+
+    const changePath = (path) =>{
+        location('/' + path)
+    }
 
     const handleNext = () => {
         if (stepNo < 3) {
@@ -30,7 +37,7 @@ const Registration = () => {
             case 1:
                 return (
                     <>
-                        <h2>Select User Type</h2>
+                        <h2 className='h11'>Select User Type</h2>
 
                         <div className='userL'>
 
@@ -38,26 +45,129 @@ const Registration = () => {
                             <img onClick={() => changeUserType('Institution', 'Student')} id='Institution' src={universityPic} alt='Institution' />
                         </div>
 
-                        <h3 id='userTypeD'>______________</h3>
+                        <h3 id='userTypeD'></h3>
 
+                     
                     </>
                 )
 
             case 2:
-                return (
-                    <>
-                        <h2>Step 2</h2>
+                if (userType === 'Student') {
+                    return (
+                        <>  <h2 className='stdH'>Personal Details</h2>
 
-                    </>
-                );
+                            <input placeholder='Student Name' className='inp' type="text" />
+
+                            <input placeholder='Institution Name' className='inp' type="text" />
+
+                            <select className='slct' name="gender">
+                                <option value="" disabled selected>Select Gender</option>
+                                <option value="male" >Male</option>
+                                <option value="female" >Female</option>
+                            </select>
+
+                            <select name="selYear" className='slct' id="">
+                                <option value="" disabled selected>Select Current Year</option>
+                                <option value="first" >First</option>
+                                <option value="second" >Second</option>
+                                <option value="third" >Third</option>
+                                <option value="four" >Four</option>
+                            </select>
+
+                            <select name="selInterest" className='slct' id="">
+                                <option value="" disabled selected>Select Interests</option>
+                                <option value="coding">Coding</option>
+                                <option value="mathematics">Mathematics</option>
+                                <option value="physics">Physics</option>
+                                <option value="biology">Biology</option>
+                                <option value="chemistry">Chemistry</option>
+                                <option value="literature">Literature</option>
+                                <option value="psychology">Psychology</option>
+                                <option value="languages">Languages</option>
+                                <option value="economics">Economics</option>
+                                <option value="philosophy">Philosophy</option>
+                                <option value="political-science">Political Science</option>
+                                <option value="art-history"> History</option>
+                               
+                               
+                            </select>
+
+
+
+                        </>
+
+                    );
+                }
+                else {
+                    return (
+                        <>
+                          <h2 className='stdH'>Institution Details</h2>
+
+<input placeholder='Institution Name' className='inp' type="text" />
+
+<input placeholder='Institution Grade' className='inp' type="text" />
+
+
+<input type="text" placeholder='Institution Location' className='inp' />
+
+<select className='slct' name="gender">
+    <option value="" disabled selected>Select Institution Type</option>
+    <option value="engineering" >Engineering</option>
+    <option value="management" >Management</option>
+    <option value="medical" >Medical</option>
+    <option value="arts" >Arts</option>
+    <option value="science" >Science</option>
+    <option value="commerce" >Commerce</option>
+</select>
+
+<select name="selInterest" className='slct' id="">
+    <option value="" disabled selected>Select Autonomous Status</option>
+    <option value="autonomous">Autonomous</option>
+    <option value="nonautonomous">Non Autonomous</option>
+   
+   
+</select>
+
+                        </>
+                    )
+                }
 
             case 3:
-                return (
-                    <>
-                        <h2>Step 3</h2>
+                if (userType === 'Student') {
+                    return (
+                        <>
+                        <h2 className='step3h'>
+                            Verification     </h2>
+                                <input className='inp' type="text" placeholder='Enter a Email ID' />
 
-                    </>
-                );
+                                <input className='inp' type="password" placeholder='Create a Password' id="" />
+
+                                <p className='uploadId'>Upload College ID</p>
+                                <input className='inp' type="file"  id="" />
+
+
+
+                        </>
+                    );
+                }
+                else {
+                    return (
+                        <>
+                             <h2 className='step3h'>
+                            Verification     </h2>
+                                <input className='inp' type="text" placeholder='College Official Email ID' />
+
+                                <input className='inp' type="password" placeholder='Create a Password' id="" />
+
+                                <p className='uploadId'>Upload College Document</p>
+                                <input className='inp' type="file"  id="" />
+
+                   
+
+                        
+                        </>
+                    )
+                }
             default:
                 return null;
         }
@@ -94,6 +204,8 @@ const Registration = () => {
                     <button onClick={handlePrev}>Previous</button>
                     <button onClick={handleNext}>Next</button>
                 </div>
+
+                <p className='alreadyR'>Already Registered?<span onClick={() => changePath('login')}> Login</span> </p>
             </div>
         </div>
     );
